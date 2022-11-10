d5142f2f2ebf7159a73d4f1de3b5c2056c29c113
gammelalf <gammelalf@hopfen.space>
2022-11-11 00:04:09 +0100

    Added new measured handler which counts the number of tags

--- src/measurements.rs
+++ src/measurements.rs
@@ -1,12 +1,13 @@
 //! Collection of measurements useful for debugging and "primitive benchmarking"
+use std::fmt::Debug;
+use std::ops::AddAssign;
+use std::time::{Duration, Instant};
 
 use libosmium::{Area, Handler, Node, Way};
-use std::ops::{AddAssign, Div};
-use std::time::{Duration, Instant};
 
 /// Generates the fields and methods common to any measured handler
 macro_rules! measured_handler {
-    ($(#[doc = $doc:literal])* pub struct $name:ident<$T:ty>) => {
+    ($(#[doc = $doc:literal])* pub struct $name:ident<H: Handler> {$T:ty}) => {
         $(#[doc = $doc])*
         pub struct $name<H: Handler> {
             /// [Handler] to be measured
@@ -24,6 +25,14 @@ macro_rules! measured_handler {
                 }
             }
 
+            /// Print the measured results
+            pub fn print(&self) {
+                let CombinedMeasure { areas, nodes, ways } = &self.measurements;
+                for (name, measurement) in [("Areas", areas), ("Nodes", nodes), ("Ways", ways)] {
+                    Self::print_single(name, measurement);
+                }
+            }
+
             /// Unwrap the measured handler, dropping the measurements
             pub fn into_handler(self) -> H {
                 self.handler
@@ -34,14 +43,16 @@ macro_rules! measured_handler {
 
 measured_handler! {
     /// measures the time required to process the various types.
-    pub struct TimedHandler<Duration>
+    pub struct TimedHandler<H: Handler> {Duration}
 }
 impl<H: Handler> TimedHandler<H> {
-    pub fn print(&self) {
-        let CombinedMeasure { areas, nodes, ways } = &self.measurements;
-        eprintln!("Areas: #{} @ {:?} each", areas.number, areas.avg());
-        eprintln!("Nodes: #{} @ {:?} each", nodes.number, nodes.avg());
-        eprintln!("Ways: #{} @ {:?} each", ways.number, ways.avg());
+    fn print_single(name: &str, measurement: &Measurement<Duration>) {
+        eprintln!(
+            "{} {name} took {:?} to process at {:?} each",
+            measurement.number,
+            measurement.acc,
+            measurement.avg()
+        );
     }
 }
 impl<H: Handler> Handler for TimedHandler<H> {
@@ -64,8 +75,45 @@ impl<H: Handler> Handler for TimedHandler<H> {
     }
 }
 
+measured_handler! {
+    /// counts the number of tags on per object.
+    pub struct TagCountedHandler<H: Handler> {u32}
+}
+impl<H: Handler> Handler for TagCountedHandler<H> {
+    fn area(&mut self, area: &Area) {
+        self.measurements
+            .areas
+            .add(area.tags().into_iter().count() as u32);
+        self.handler.area(area);
+    }
+
+    fn node(&mut self, node: &Node) {
+        self.measurements
+            .nodes
+            .add(node.tags().into_iter().count() as u32);
+        self.handler.node(node);
+    }
+
+    fn way(&mut self, way: &Way) {
+        self.measurements
+            .ways
+            .add(way.tags().into_iter().count() as u32);
+        self.handler.way(way);
+    }
+}
+impl<H: Handler> TagCountedHandler<H> {
+    fn print_single(name: &str, measurement: &Measurement<u32>) {
+        eprintln!(
+            "{name} have between {} and {} tags, averaging at {}",
+            measurement.min,
+            measurement.max,
+            measurement.avg()
+        );
+    }
+}
+
 /// Collective measurements of a single type seperated in areas, nodes and ways.
-#[derive(Default)]
+#[derive(Default, Copy, Clone, Debug)]
 pub struct CombinedMeasure<T: Measureable> {
     pub areas: Measurement<T>,
     pub nodes: Measurement<T>,
@@ -111,17 +159,45 @@ impl<T: Measureable> Measurement<T> {
     }
 
     /// Get the average value
-    pub fn avg(&self) -> <T as Div<u32>>::Output
-    where
-        T: Div<u32>,
-    {
-        self.acc / self.number
+    pub fn avg(&self) -> T::Avg {
+        self.acc.avg(self.number)
+    }
+}
+// Custom Debug impl
+// - also outputs `avg`
+impl<T: Measureable> Debug for Measurement<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("Measurement")
+            .field("number", &self.number)
+            .field("acc", &self.acc)
+            .field("min", &self.min)
+            .field("max", &self.max)
+            .field("avg", &self.avg())
+            .finish()
     }
 }
 
 /// Empty trait combining all traits required of measured values into a single shorthand.
-pub trait Measureable: Default + Copy + PartialOrd + AddAssign<Self> {}
-impl<T: Default + Copy + PartialOrd + AddAssign<T>> Measureable for T {}
-
-pub struct Time;
-pub struct TagCount;
+pub trait Measureable: Default + Copy + PartialOrd + AddAssign<Self> + Debug {
+    type Avg: Debug;
+    fn avg(self, count: u32) -> Self::Avg;
+}
+impl Measureable for Duration {
+    type Avg = Self;
+    fn avg(self, count: u32) -> Self::Avg {
+        self / count as u32
+    }
+}
+macro_rules! impl_numeric {
+    ($($T:ty),*) => {
+        $(
+            impl Measureable for $T {
+                type Avg = f64;
+                fn avg(self, count: u32) -> Self::Avg {
+                    self as f64 / count as f64
+                }
+            }
+        )*
+    };
+}
+impl_numeric!(u32, usize);
