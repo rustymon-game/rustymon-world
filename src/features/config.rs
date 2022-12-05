a6226cb28d0b3b258218ac7fa53d9b223f5d69e2
gammelalf <gammelalf@hopfen.space>
2022-12-05 21:10:42 +0100

    Added id aliases to config language

--- src/features/config.rs
+++ src/features/config.rs
@@ -1,5 +1,5 @@
 //! Ast and grammar for the custom config language
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::hash::Hash;
 use std::marker::PhantomData;
 
@@ -124,25 +124,40 @@ where
     }
 
     fn handle_file(&mut self, file: Pair<'i, Rule>) -> ParserResult<Ast<T>> {
+        let mut area_aliases: HashMap<&'i str, usize> = HashMap::new();
+        let mut node_aliases: HashMap<&'i str, usize> = HashMap::new();
+        let mut way_aliases: HashMap<&'i str, usize> = HashMap::new();
         Ok(match file.as_rule() {
             Rule::file => {
                 let mut areas = None;
                 let mut nodes = None;
                 let mut ways = None;
                 for block in file.into_inner() {
-                    let (title, statements) = block.head_tail().ok_or(MISSING)?;
-                    let block = match title.as_rule() {
-                        Rule::areas => &mut areas,
-                        Rule::nodes => &mut nodes,
-                        Rule::ways => &mut ways,
-                        _ => return Err(ParserError::InvalidRule),
+                    let rule = block.as_rule();
+                    match rule {
+                        Rule::block => (),
+                        Rule::EOI => continue,
+                        _ => return invalid_rule(rule, [Rule::block, Rule::EOI]),
+                    }
+
+                    let (title, statements) = block.head_tail().ok_or(missing_child(rule))?;
+                    let (block, aliases) = match title.as_rule() {
+                        Rule::areas => (&mut areas, &mut area_aliases),
+                        Rule::nodes => (&mut nodes, &mut node_aliases),
+                        Rule::ways => (&mut ways, &mut way_aliases),
+                        invalid => {
+                            return Err(ParserError::InvalidRule(
+                                invalid,
+                                vec![Rule::areas, Rule::nodes, Rule::ways],
+                            ))
+                        }
                     };
                     if block.is_none() {
-                        *block = Some(
-                            statements
-                                .map(|rule| self.handle_statement(rule))
-                                .collect::<ParserResult<_>>()?,
-                        );
+                        let mut branches = Vec::new();
+                        for rule in statements {
+                            self.handle_statement(rule, &mut branches, aliases)?;
+                        }
+                        *block = Some(branches)
                     } else {
                         return Err(ParserError::DuplicateBlocks);
                     }
@@ -153,28 +168,55 @@ where
                     ways: ways.unwrap_or_default(),
                 }
             }
-            _ => return Err(ParserError::InvalidRule),
+            i => return invalid_rule(i, [Rule::file]),
         })
     }
 
-    fn handle_statement(&mut self, stmnt: Pair<'i, Rule>) -> ParserResult<Branch<T>> {
-        Ok(match stmnt.as_rule() {
+    fn handle_statement(
+        &mut self,
+        stmnt: Pair<'i, Rule>,
+        branches: &mut Vec<Branch<T>>,
+        aliases: &mut HashMap<&'i str, usize>,
+    ) -> ParserResult<()> {
+        let rule = stmnt.as_rule();
+        match rule {
             Rule::statement => {
-                let [id, expr] = stmnt.children().ok_or(MISSING)?;
-                Branch {
-                    id: id.as_str().parse().unwrap(),
-                    expr: self.handle_expr(expr)?,
-                }
+                self.handle_statement(stmnt.child().ok_or(missing_child(rule))?, branches, aliases)?
             }
-            _ => return Err(ParserError::InvalidRule),
-        })
+            Rule::alias => {
+                let [identifier, number] = stmnt.children().ok_or(missing_child(rule))?;
+                aliases.insert(identifier.as_str(), number.as_str().parse().unwrap());
+            }
+            Rule::branch => {
+                let [result, expr] = stmnt.children().ok_or(missing_child(rule))?;
+                let id = match result.as_rule() {
+                    Rule::number => result.as_str().parse().unwrap(),
+                    Rule::identifier => *aliases
+                        .get(result.as_str())
+                        .ok_or(ParserError::UnknownAlias(result.as_str().to_string()))?,
+                    _ => return invalid_rule(result.as_rule(), [Rule::number, Rule::identifier]),
+                };
+                let branch = Branch {
+                    id,
+                    expr: self.handle_expr(expr)?,
+                };
+                branches.push(branch);
+            }
+            _ => return invalid_rule(rule, [Rule::statement, Rule::alias, Rule::branch]),
+        }
+        Ok(())
     }
 
     fn handle_expr(&mut self, expr: Pair<'i, Rule>) -> ParserResult<Expr<T>> {
-        Ok(match expr.as_rule() {
-            Rule::expr => self.handle_expr(expr.child().ok_or(MISSING)?)?,
-            Rule::lookup => Expr::Lookup(self.handle_lookup(expr.child().ok_or(MISSING)?)?),
-            Rule::not => Expr::Not(Box::new(self.handle_expr(expr.child().ok_or(MISSING)?)?)),
+        let rule = expr.as_rule();
+        Ok(match rule {
+            Rule::expr => self.handle_expr(expr.child().ok_or(missing_child(rule))?)?,
+            Rule::lookup => {
+                Expr::Lookup(self.handle_lookup(expr.child().ok_or(missing_child(rule))?)?)
+            }
+            Rule::not => Expr::Not(Box::new(
+                self.handle_expr(expr.child().ok_or(missing_child(rule))?)?,
+            )),
             Rule::or => Expr::Or(
                 expr.into_inner()
                     .map(|rule| self.handle_expr(rule))
@@ -185,25 +227,31 @@ where
                     .map(|rule| self.handle_expr(rule))
                     .collect::<ParserResult<_>>()?,
             ),
-            _ => return Err(ParserError::InvalidRule),
+            _ => {
+                return invalid_rule(
+                    rule,
+                    [Rule::expr, Rule::lookup, Rule::not, Rule::or, Rule::and],
+                )
+            }
         })
     }
 
     fn handle_lookup(&mut self, lookup: Pair<'i, Rule>) -> ParserResult<Lookup<T>> {
+        let rule = lookup.as_rule();
         Ok(match lookup.as_rule() {
-            Rule::lookup => self.handle_lookup(lookup.child().ok_or(MISSING)?)?,
+            Rule::lookup => self.handle_lookup(lookup.child().ok_or(missing_child(rule))?)?,
             Rule::any => Lookup::Any {
-                key: self.handle_string(lookup.child().ok_or(MISSING)?)?,
+                key: self.handle_string(lookup.child().ok_or(missing_child(rule))?)?,
             },
             Rule::single => {
-                let [key, value] = lookup.children().ok_or(MISSING)?;
+                let [key, value] = lookup.children().ok_or(missing_child(rule))?;
                 Lookup::Single {
                     key: self.handle_string(key)?,
                     value: self.handle_string(value)?,
                 }
             }
             Rule::list => {
-                let (key, values) = lookup.head_tail().ok_or(MISSING)?;
+                let (key, values) = lookup.head_tail().ok_or(missing_child(rule))?;
                 Lookup::List {
                     key: self.handle_string(key)?,
                     values: values
@@ -211,18 +259,17 @@ where
                         .collect::<ParserResult<_>>()?,
                 }
             }
-            _ => return Err(ParserError::InvalidRule),
+            _ => return invalid_rule(rule, [Rule::lookup, Rule::any, Rule::single, Rule::list]),
         })
     }
 
     fn handle_string(&mut self, string: Pair<'i, Rule>) -> ParserResult<T> {
-        Ok(match string.as_rule() {
-            Rule::string | Rule::inner => {
-                let string = string.as_str();
-                (self.convert_string)(string)
-            }
-            _ => return Err(ParserError::InvalidRule),
-        })
+        let inner = match string.as_rule() {
+            Rule::string => string.child().ok_or(missing_child(Rule::string))?,
+            Rule::inner => string,
+            _ => return invalid_rule(string.as_rule(), [Rule::string, Rule::inner]),
+        };
+        Ok((self.convert_string)(inner.as_str()))
     }
 }
 
@@ -234,20 +281,62 @@ pub enum ParserError {
     /// A block appeared twice
     DuplicateBlocks,
 
+    /// An alias was used before it was declared
+    UnknownAlias(String),
+
     /// A rule is missing a specific child, whose existence should be guaranteed by the grammar
     ///
     /// This is to be treated as a mistake in this library.
-    MissingChild,
+    MissingChild(Rule),
 
     /// A `ConfigParser::handle_*` function got a rule it can't handle
     ///
     /// This is to be treated as a mistake in this library.
-    InvalidRule,
+    InvalidRule(Rule, Vec<Rule>),
 }
-const MISSING: ParserError = ParserError::MissingChild;
 impl From<pest::error::Error<Rule>> for ParserError {
     fn from(error: pest::error::Error<Rule>) -> Self {
         Self::SyntaxError(error)
+    }
+}
+fn invalid_rule<T, const N: usize>(got: Rule, expected: [Rule; N]) -> ParserResult<T> {
+    Err(ParserError::InvalidRule(got, expected.to_vec()))
+}
+fn missing_child(parent: Rule) -> ParserError {
+    ParserError::MissingChild(parent)
+}
+impl std::fmt::Display for ParserError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            ParserError::SyntaxError(err) => err.fmt(f),
+            ParserError::DuplicateBlocks => write!(f, "Some blocks where specified twice"),
+            ParserError::MissingChild(parent) => {
+                write!(
+                    f,
+                    "This error should never happen! Please tell the maintainer!\n"
+                )?;
+                write!(f, "Parent: {:?}", parent)
+            }
+            ParserError::InvalidRule(got, exp) => {
+                write!(
+                    f,
+                    "This error should never happen! Please tell the maintainer!\n"
+                )?;
+                write!(f, "Got: {:?}\n", got)?;
+                if exp.len() == 1 {
+                    write!(f, "Expected: {:?}\n", exp[1])
+                } else {
+                    write!(f, "Expected one of:\n")?;
+                    for rule in exp {
+                        write!(f, "- {:?}\n", rule)?;
+                    }
+                    Ok(())
+                }
+            }
+            ParserError::UnknownAlias(alias) => {
+                write!(f, "The alias \"{alias}\" was used before its declaration")
+            }
+        }
     }
 }
 
